20f26306ac0aaff796f57ac70934da4ae7321458
Angel <matamorosfernandez@gmail.com>
2025-02-07 15:37:16 -0300

    TIPOS BASICOS1

--- 01-tipos/09-argumentos_parametros.js
+++ 01-tipos/09-argumentos_parametros.js
@@ -1,9 +1,9 @@
 function suma(a,b){
     console.log(arguments);
-    return a + b;                //parametros
+    return a + b;                               //parametros
 }
-                                               //las funciones pueden recibir multiples Arg.  y Parm.
+                                                //las funciones pueden recibir multiples Arg.  y Parm.
 
-let resultado = suma(5, 6, 2, 3, 1);       //argumentos
+let resultado = suma(5, 6, 2, 3, 1);            //argumentos
 console.log(resultado);
 console.log(typeof suma);
